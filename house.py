fe8cf8fa761887c6c776a546c62c1f681b3e16a6
Issa Chiragov <chiragov1994@gmail.com>
2022-11-04 11:32:11 +0300

    Add roof, walls and foundation

--- house.py
+++ house.py
@@ -1,4 +1,10 @@
 
+def main ():
+    x, y = 300, 400
+    width, height = 200, 300
+
+    draw_house(x, y, width, height)
+
 def draw_house(x, y, width, height):
     """
     Function is drawing house all width and height from reference point (x, y),
@@ -9,11 +15,31 @@ def draw_house(x, y, width, height):
     :param height: It is all height the house include fundamental and roof
     :return: None
     """
-
     print('Типа рисую домик...', x, y, width, height)
-    pass # do nothing
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
 
-x, y = 100, 100
-width, height = 200, 200
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
 
-draw_house(x, y, width, height)
+
+
+def draw_house_foundation(x, y, width, height):
+    print('Типа рисую фундамент...', x, y, width, height)
+    pass
+
+
+def draw_house_walls(x, y, width, height):
+    print('Типа рисую стены...', x, y, width, height)
+    pass
+
+
+def draw_house_roof(x, y, width, height):
+    print('Типа рисую крышу...', x, y, width, height)
+    pass
+
+
+main()
