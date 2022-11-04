2339c28ad3a8b8532bc99f9cb2c8d4c2100819d3
Issa Chiragov <chiragov1994@gmail.com>
2022-11-04 10:00:12 +0300

    My work.

---
+++ house.py
@@ -0,0 +1,19 @@
+
+def draw_house(x, y, width, height):
+    """
+    Function is drawing house all width and height from reference point (x, y),
+    which is in the middle lower point of fundamental
+    :param x: It is the middle of the house
+    :param y: It is the lower of the house fundamental
+    :param width: It is all width the house include fundamental and roof
+    :param height: It is all height the house include fundamental and roof
+    :return: None
+    """
+
+    print('Типа рисую домик...', x, y, width, height)
+    pass # do nothing
+
+x, y = 100, 100
+width, height = 200, 200
+
+draw_house(x, y, width, height)
